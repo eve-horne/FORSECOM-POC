a2ea79adec0c69cb7093026817bb8b32dbbaecbd
mirrorkeydev <mirrorkeydev@gmail.com>
2020-04-29 12:20:26 -0700

    feat(vuex): added watcher to let Vue ping C# when the login status changes

--- ForsecomInterops/ForsecomInterops/Form1.Designer.cs
+++ ForsecomInterops/ForsecomInterops/Form1.Designer.cs
@@ -49,7 +49,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(303, 364);
+            this.button1.Location = new System.Drawing.Point(572, 323);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(207, 23);
             this.button1.TabIndex = 1;
